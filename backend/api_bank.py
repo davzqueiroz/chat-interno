2094652f6af1f2bc3199e9616da3c16e5387e039
Davi Oliveira <146399477+davzqueiroz@users.noreply.github.com>
2024-10-02 13:58:54 -0300

    Add files via upload

--- backend/api_bank.py
+++ backend/api_bank.py
@@ -18,40 +18,58 @@ def connection():
     return conn
 
 
-with connection() as conn:
-    user_1 = 'davi'
-    user_2 = 'ivan'
-    cursor = conn.cursor()
-    mensagens = cursor.execute(f"SELECT * FROM MENSAGENS WHERE DE = '{user_1}' or DE = '{user_2}' AND PARA = '{user_1}' or PARA = '{user_2}' ORDER BY DIA_HORA ASC").fetchall()
-    print(mensagens)
-
-exit()
-
 app = Flask(__name__)
 CORS(app)
 
 
 @app.route("/get_messages", methods=[''])
-def get_messages():
+def consultar(my_id, is_group, id_target=None, group_name=None):
+    if type(my_id) is not int: raise Exception('Parâmetro MY_ID precisa ser INT')
+    elif type(is_group) is not bool: raise Exception('Parâmetro IS_GROUP precisa ser Bool')
+    elif id_target is not None and type(id_target) is not int: raise Exception('Parâmetro ID_TARGET precisa ser INT')
+
     with connection() as conn:
         cursor = conn.cursor()
-        mensagens = cursor.execute("SELECT * FROM MENSAGENS").fetchall()
-        print(mensagens)
+        # ============================= CONSULTA CONVERSAS EM QUE MY_ID PARTICIPA ================================
+        conversas_participantes = cursor.execute(f"SELECT CONVERSATION_ID FROM CONVERSATION_MEMBERS WHERE MEMBER_ID = {my_id}").fetchall()
+        conversas_participantes = ','.join(map(str, [t[0] for t in conversas_participantes]))
+        if len(conversas_participantes) == 0: return []
 
-    return mensagens
+        # =============================== SE FOR GRUPO, CONSULTA O CONVERSATION_ID PELO NOME DO GRUPO ================
+        if is_group:
+
+            group_id = cursor.execute(f"SELECT CONVERSATION_ID FROM CONVERSATIONS WHERE CONVERSATION_NAME = '{group_name}' AND CONVERSATION_ID IN ({conversas_participantes})").fetchone()
+            conversation_target = group_id[0] if group_id is not None else 0
+
+        # ============ SE NAO FOR GRUPO, EXCLUI OS GRUPOS DA LISTA E CONSULTA O CONVERSATION ID DO PRIVADO =============
+        elif not is_group and id_target is not None:
+            consulta = cursor.execute(f"SELECT CONVERSATION_ID FROM CONVERSATION_MEMBERS WHERE MEMBER_ID = {my_id} AND CONVERSATION_ID IN (SELECT CONVERSATION_ID FROM CONVERSATION_MEMBERS WHERE MEMBER_ID = {id_target})").fetchall()
+            consulta = ','.join(map(str, [t[0] for t in consulta]))
+            id_pvs = cursor.execute(f"SELECT CONVERSATION_ID FROM CONVERSATIONS WHERE IS_GROUP = 0 AND CONVERSATION_ID IN ({consulta})").fetchone()
+            conversation_target = id_pvs[0] if id_pvs is not None else 0
+
+        if conversation_target == 0: return []
+
+        consulta_mensagens = cursor.execute(f"SELECT * FROM MESSAGES WHERE CONVERSATION_ID = {conversation_target} ORDER BY SENT_AT").fetchall()
+        mensagens = []
+        for mensagem in consulta_mensagens:
+            mensagens.append({'id': mensagem[0], 'conversation_id': mensagem[1], 'sender_id': mensagem[2], 'content': mensagem[3], 'sent_at': mensagem[4], 'type': mensagem[5]})
+        return mensagens
+
+        # print(cursor.execute(f"SELECT CONVERSATION_ID FROM CONVERSATIONS WHERE IS_GROUP = {is_group} AND CONVERSATION_ID IN ({retorno})").fetchall())
 
 
 @app.route("/get_contacts", methods=['GET'])
 def get_contacts():
     with connection() as conn:
         cursor = conn.cursor()
-        contacts = cursor.execute("SELECT NOME FROM USUARIOS").fetchall()
+        contacts = cursor.execute("SELECT USER_ID, NOME FROM USUARIOS").fetchall()
         contacts_list = []
         for contact in contacts:
-            contacts_list.append(contact[0])
+            contacts_list.append(contact[1])
         contacts_json = {'nomes': contacts_list}
         contacts_json = json.dumps(contacts_json)
     return contacts_json
 
 
-app.run(port=5000, host='localhost', debug=True)
+app.run(port=5000, host='192.168.0.37', debug=True)
