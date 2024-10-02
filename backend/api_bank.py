8654589b1b43ac743d6009abcd50ed66b48de1bc
Davi Oliveira <146399477+davzqueiroz@users.noreply.github.com>
2024-10-01 22:17:44 -0300

    Add files via upload

--- backend/api_bank.py
+++ backend/api_bank.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS, cross_origin
 import pyodbc
 import json
+import sqlite3
 
 SERVER = '192.168.0.169,1433'
 DATABASE = 'DaviTalk'
@@ -10,19 +11,34 @@ PASSWORD = '12345678@'
 
 
 def connection():
-    connectionString = f'DRIVER={{ODBC Driver 13 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}'
-    conn = pyodbc.connect(connectionString)
-    conn.autocommit = True
+    # connectionString = f'DRIVER={{ODBC Driver 13 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}'
+    # conn = pyodbc.connect(connectionString)
+    # conn.autocommit = True
+    conn = sqlite3.connect('bank_webchat')
     return conn
 
+
+with connection() as conn:
+    user_1 = 'davi'
+    user_2 = 'ivan'
+    cursor = conn.cursor()
+    mensagens = cursor.execute(f"SELECT * FROM MENSAGENS WHERE DE = '{user_1}' or DE = '{user_2}' AND PARA = '{user_1}' or PARA = '{user_2}' ORDER BY DIA_HORA ASC").fetchall()
+    print(mensagens)
+
+exit()
 
 app = Flask(__name__)
 CORS(app)
 
 
-@app.route("/", methods=[''])
-def insert_message():
-    return None
+@app.route("/get_messages", methods=[''])
+def get_messages():
+    with connection() as conn:
+        cursor = conn.cursor()
+        mensagens = cursor.execute("SELECT * FROM MENSAGENS").fetchall()
+        print(mensagens)
+
+    return mensagens
 
 
 @app.route("/get_contacts", methods=['GET'])
