df6fe8e9d83e7a20bfff56b88b6c42a8e8b088ec
Davi Oliveira <146399477+davzqueiroz@users.noreply.github.com>
2024-10-02 22:37:28 -0300

    Add files via upload

--- frontend/chat_page/chat.js
+++ frontend/chat_page/chat.js
@@ -1,52 +1,58 @@
+const messages_contacts = new Map();
+const lista_mensagens = document.getElementById('lista-mensagens');
+const botao_enviar = document.getElementById('botao-enviar');
+const input_message = document.getElementById('input-message');
+
 /* Função para alterar entre contatos e configurações */
 
 function togglePanel() {
-    const contactPanel = document.getElementById('contactPanel');
-    const settingsPanel = document.getElementById('settingsPanel');
-    
-    if (contactPanel.classList.contains('active')) {
-        contactPanel.classList.remove('active');
-        settingsPanel.classList.add('active');
-    } else {
-        settingsPanel.classList.remove('active');
-        contactPanel.classList.add('active');
-    }
+	const contactPanel = document.getElementById('contactPanel');
+	const settingsPanel = document.getElementById('settingsPanel');
+
+	if (contactPanel.classList.contains('active')) {
+		contactPanel.classList.remove('active');
+		settingsPanel.classList.add('active');
+	} else {
+		settingsPanel.classList.remove('active');
+		contactPanel.classList.add('active');
+	}
 }
 
 /* Função para inserir mensagens no HTML */
 
-function insertMessageHTML() {
-    const value_textbar = document.getElementById('input-message').value;    
-    const mensagem = document.createElement('li')
+function insertMessageHTML(contato) {
+	console.log(contato);
+	const value_textbar = document.getElementById('input-message').value;
+	const mensagem = document.createElement('li');
 
-    if (value_textbar.trim() !== '') {
-    mensagem.innerText = value_textbar
-    mensagem.classList.add('sended')
+	if (value_textbar.trim() !== '') {
+		mensagem.innerText = value_textbar;
+		mensagem.classList.add('sended');
 
-    document.getElementById('lista-mensagens').appendChild(mensagem)
+		document.getElementById('lista-mensagens').appendChild(mensagem);
 
-    const messageContainer = document.querySelector('.messages-chat')
-    messageContainer.scrollTop = messageContainer.scrollHeight;
-    }
+		const messageContainer = document.querySelector('.messages-chat');
+		messageContainer.scrollTop = messageContainer.scrollHeight;
+	}
 
-    document.getElementById('input-message').value = '';
+	document.getElementById('input-message').value = '';
+
+	if (!messages_contacts.has(contato['id'])) messages_contacts.set(contato['id'], []);
+	const history = messages_contacts.get(contato['id']);
+	history.push({
+		id: 3,
+		conversation_id: 3,
+		sender_id: 1,
+		content: value_textbar,
+	});
+	// messages_contacts.set(contato['id'], history)
 }
-
-/* Evento pra enviar mensagem pressionando */
-
-document.getElementById('input-message').addEventListener("keydown", function(event){
-    if (event.key == 'Enter' && !event.shiftKey) {
-        event.preventDefault()
-        insertMessageHTML()
-    }
-});
 
 /* Foto dinamica */
 
-document.addEventListener("DOMContentLoaded", async(event) => { 
-    const foto_user = document.getElementById('foto-usuario');
-    foto_user.src = "https://github.com/davzqueiroz.png"
-    
+document.addEventListener('DOMContentLoaded', async (event) => {
+	const foto_user = document.getElementById('foto-usuario');
+	foto_user.src = 'https://github.com/davzqueiroz.png';
 });
 
 /* Contatos dinâmicos */
@@ -54,40 +60,82 @@ document.addEventListener("DOMContentLoaded", async(event) => {
 const lista = document.getElementById('lista-contatos');
 
 async function get_contacts() {
-    try {
-        const response = await fetch('http://192.168.0.37:5000/get_contacts');
-        if (!response.ok) throw new Error('Network response was not ok');
-        const data = await response.json()
-        console.log(data)
-        data['nomes'].forEach(element => {
-            const contato = document.createElement('li')
-            contato.innerText = element
-            contato.addEventListener('click', function editContactName() {
-                document.getElementById('nome-contato').innerText = element
-                showMessages(element)
-            })
-            lista.appendChild(contato)
-        });
-    } catch (error) {
-        console.log(error)
-    }
+	try {
+		const response = await fetch('http://localhost:5000/get_contacts');
+		if (!response.ok) throw new Error('Network response was not ok');
+		const data = await response.json();
+		console.log(data);
+
+		for (const contact of data) {
+			if (contact['id'] == 1) continue;
+			console.log(contact);
+			const contato = document.createElement('li');
+			contato.innerText = contact['nome'];
+			contato.addEventListener('click', function editContactName() {
+				document.getElementById('nome-contato').innerText = contact['nome'];
+				showMessages(contact);
+				botao_enviar.onclick = function () {
+					insertMessageHTML(contact);
+				};
+				input_message.onkeydown = (event) => {
+					console.log(event);
+					if (event.key == 'Enter' && !event.shiftKey) {
+						event.preventDefault();
+						insertMessageHTML(contact);
+					}
+				};
+			});
+
+			lista.appendChild(contato);
+		}
+	} catch (error) {
+		console.log(error);
+	}
 }
 
-get_contacts()
+get_contacts();
 
 /* Mensagens dinâmicas */
 
-async function showMessages(contactName) {
-    try {
-        const response = await fetch('http://192.168.0.37:5000/get_messages');
-        if (!response.ok) {throw new Error('Network response was not ok')};
-        const data = await response.json()
-        data['content'].forEach(element => {
-            /* Adicionar aqui dentro lógica para inserir mensagens no HTML*/
-        });
-    } catch (error) {
-        console.log(error);
-    }
+async function showMessages(contact) {
+	lista_mensagens.innerText = '';
+	if (messages_contacts.has(contact['id'])) {
+		render_messages(messages_contacts.get(contact['id']));
+		return;
+	}
+	try {
+		const response = await fetch('http://localhost:5000/get_messages', {
+			method: 'POST',
+			body: JSON.stringify({
+				my_id: 1,
+				is_group: false,
+				id_target: contact['id'],
+				group_name: '',
+			}),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+		if (!response.ok) {
+			throw new Error('Network response was not ok');
+		}
+		const data = await response.json();
+		messages_contacts.set(contact['id'], data);
+		render_messages(data);
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+/* RENDERIZAR AS MENSAGENS NO HTML */
+
+function render_messages(messages) {
+	messages.forEach((element) => {
+		const msg = document.createElement('li');
+		msg.classList.add(element['sender_id'] == 1 ? 'sended' : 'received');
+		msg.innerText = element['content'];
+		lista_mensagens.appendChild(msg);
+	});
 }
 
 /* ============================== PAINEL DE ANEXO =========================== */
@@ -101,7 +149,7 @@ async function showMessages(contactName) {
 // document.addEventListener("click", function(event) {
 //     const panel = document.getElementById("attachmentPanel");
 //     const button = document.querySelector("image-anexo");
-  
+
 //     // Verifica se o clique foi fora do painel e fora do botão de anexo
 //     if (!panel.contains(event.target) && !button.contains(event.target)) {
 //       panel.classList.remove("open"); // Fecha o painel
