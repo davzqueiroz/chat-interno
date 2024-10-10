b8314200c5900619654139a3e999979bc8b113ce
Davi Oliveira <146399477+davzqueiroz@users.noreply.github.com>
2024-10-10 13:46:11 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
 import bcrypt
-import jwt
 from datetime import datetime, timedelta
-from flask import Flask, request, jsonify, Response, render_template
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 from backend.functions import *
-from backend.hash_map import Hash_map
+from backend.hashmap import HashMap
 import socketio
 import eventlet
 import socket
-import time
 
-hostname = socket.gethostname()
-local_ip = socket.gethostbyname(hostname)
-client_connections = Hash_map()
+local_ip = socket.gethostbyname(socket.gethostname())
+client_connections = HashMap()
 
 # =================================================== SOCKET.IO ========================================================
 # =================================================== SOCKET.IO ========================================================
@@ -29,7 +26,7 @@ def connect(sid, environ):
         if 'Authorization' in tupla:
             token = tupla[1]
 
-    user_data = verificar_token(token)
+    user_data = verify_token(token)
     user_data['sid'] = sid
     client_connections.set(user_data["id"], user_data)
     sio.emit('receive_client_connections', client_connections.get_connections_name())
@@ -46,21 +43,27 @@ def send_message(sid, data):
     target_id = data['target_id']
     message = data['message']
     author_id = data['author_id']
-    conversation_id = consulta_conversation_id(author_id, target_id)
 
-    inserir_mensagem(conversation_id, author_id, message)
+    if data['is_group'] is False:
+        conversation_id = consulta_conversation_id(author_id, target_id)
+        insert_message(conversation_id, author_id, message)
+        if client_connections.has(target_id):
+            data = {'target_id': target_id, 'message': message, 'author': sid, 'author_id': author_id, 'author_name': consulta_nome(author_id)}
+            sio.emit("message", data)
 
-    if client_connections.has(target_id):
-        data = {'target_id': target_id, 'message': message, 'author': sid, 'author_id': author_id}
+    elif data['is_group'] is True:
+        insert_message(target_id, author_id, message)
+        data = {'target_id': target_id, 'message': message, 'author': sid, 'author_id': author_id, 'author_name': consulta_nome(author_id)}
         sio.emit("message", data)
 
 
+
+
 # ===================================================== FLASK ==========================================================
 # ===================================================== FLASK ==========================================================
 # ===================================================== FLASK ==========================================================
 
 
-# Criação da aplicação Flask
 app = Flask(__name__)
 app.config['SECRET_KE'] = 'secret!'
 CORS(app)
@@ -84,9 +87,9 @@ def chat():
 @app.route("/login", methods=['POST'])
 def login():
     data = request.get_json()
-    email, senha = data['email'], data['senha']
+    email, password = data['email'], data['senha']
     if type(email) is not str: return jsonify({'error': 'Email precisa ser STRING'}), 401
-    if type(senha) is not str: return jsonify({'error': 'Senha precisa ser STRING'}), 401
+    if type(password) is not str: return jsonify({'error': 'Senha precisa ser STRING'}), 401
 
     with connection() as conn:
         cursor = conn.cursor()
@@ -94,7 +97,7 @@ def login():
         if consulta_usuario is None:
             return jsonify({'error': 'Usuário e/ou senha incorreta'}), 401
         else:
-            if bcrypt.checkpw(senha.encode(), consulta_usuario[2]):
+            if bcrypt.checkpw(password.encode(), consulta_usuario[2]):
                 expiration = datetime.now() + timedelta(hours=9)
                 token = jwt.encode(payload={'id': consulta_usuario[0], 'nome': consulta_usuario[3], 'exp': expiration},
                                    key='webchat', algorithm='HS256')
@@ -111,7 +114,7 @@ def login():
 @app.route("/get_messages", methods=['POST'])
 def get_messages():
     token = request.headers['Authorization'].replace('Bearer ', '').strip()
-    user_data = verificar_token(token)
+    user_data = verify_token(token)
 
     if user_data is not None:
         is_group = request.get_json()['is_group']
@@ -119,7 +122,8 @@ def get_messages():
         group_name = request.get_json()['group_name']
 
         return consultar_mensagens(user_data['id'], is_group, id_target, group_name)
-    else: return jsonify({'error': 'Token inválido'}), 401
+    else:
+        return jsonify({'error': 'Token inválido'}), 401
 
 
 # =============================================== ROTA DE GRUPOS =======================================================
@@ -130,17 +134,21 @@ def get_messages():
 @app.route("/get_groups", methods=['GET'])
 def get_groups():
     token = request.headers['Authorization'][7:]
-    user_data = verificar_token(token)
+    user_data = verify_token(token)
 
     if user_data is not None:
         with connection() as conn:
             cursor = conn.cursor()
-            groups_list = []
-            groups = cursor.execute(f"SELECT CONVERSATION_ID, CONVERSATION_NAME FROM CONVERSATIONS WHERE IS_GROUP = 1 AND CONVERSATION_ID IN (SELECT CONVERSATION_ID FROM CONVERSATION_MEMBERS WHERE MEMBER_ID = {user_data['id']}) ORDER BY CONVERSATION_NAME ASC").fetchall()
-            for group in groups:
-                groups_list.append({'id': group[0], 'nome': group[1]})
-            return groups_list
-    else: return jsonify({'error': 'Token inválido'}), 401
+            groups = []
+            consulta = cursor.execute(
+                f"SELECT CONVERSATION_ID, CONVERSATION_NAME FROM CONVERSATIONS WHERE IS_GROUP = 1 "
+                f"AND CONVERSATION_ID IN (SELECT CONVERSATION_ID FROM CONVERSATION_MEMBERS "
+                f"WHERE MEMBER_ID = {user_data['id']}) ORDER BY CONVERSATION_NAME ASC").fetchall()
+            for group in consulta:
+                groups.append({'id': group[0], 'nome': group[1]})
+            return groups
+    else:
+        return jsonify({'error': 'Token inválido'}), 401
 
 
 # ============================================== ROTA DE CONTATOS ======================================================
@@ -151,17 +159,18 @@ def get_groups():
 @app.route("/get_contacts", methods=['GET'])
 def get_contacts():
     token = request.headers['Authorization'].replace('Bearer ', '').strip()
-    user_data = verificar_token(token)
+    user_data = verify_token(token)
 
     if user_data is not None:
         with connection() as conn:
             cursor = conn.cursor()
-            contacts = cursor.execute("SELECT USER_ID, NOME FROM USUARIOS ORDER BY NOME ASC").fetchall()
-            contacts_list = []
-            for contact in contacts:
-                contacts_list.append({'id': contact[0], 'nome': contact[1]})
-        return contacts_list
-    else: return jsonify({'error': 'Token inválido'}), 401
+            consulta = cursor.execute("SELECT USER_ID, NOME FROM USUARIOS ORDER BY NOME ASC").fetchall()
+            contacts = []
+            for contact in consulta:
+                contacts.append({'id': contact[0], 'nome': contact[1]})
+        return contacts
+    else:
+        return jsonify({'error': 'Token inválido'}), 401
 
 
 if __name__ == '__main__':
